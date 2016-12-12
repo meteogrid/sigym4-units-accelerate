8e85a5d387fcc97dccefcd777fb328fbc8df7e4a
Alberto Valverde <alberto@toscat.net>
2016-12-12 13:21:49 +0100

    docs

--- test/Sigym4/Units/Accelerate/CommonSpec.hs
+++ test/Sigym4/Units/Accelerate/CommonSpec.hs
@@ -15,10 +15,12 @@ module Sigym4.Units.Accelerate.CommonSpec ( spec, main ) where
 import           Sigym4.Units
 import           Sigym4.Units.Accelerate ()
 import           Sigym4.Units.Accelerate.Common (deriveQE)
+
+import           Control.Newtype
 import           Data.Array.Accelerate as A
 import qualified Data.Array.Accelerate.LLVM.Native      as CPU
 import           Data.List
-import           Control.Newtype
+import           Data.Word (Word8)
 import           Unsafe.Coerce
 
 import           Test.Hspec
@@ -29,6 +31,19 @@ main :: IO ()
 main = hspec spec
 
 
+
+
+-- | Dangerous claims to be a newtype of a Distance but it's not...
+newtype Dangerous = Dangerous Word8
+type instance MachineType Dangerous = MachineType Distance
+type instance Units Dangerous = Units Distance
+-- Broken and unsafe instance of Newtype, this would segfault as the word would
+-- be bitcasted to a double messing up all array bounds, etc..
+instance Newtype Dangerous Distance where
+  pack = unsafeCoerce
+  unpack = unsafeCoerce
+-- This produces code which should not typecheck
+deriveQE [t|Dangerous -> Distance|]
 
 
 spec :: Spec
@@ -42,17 +57,3 @@ spec =
     let asMeters = A.lift (Distance (2 *~ weaken meter)) :: Exp Distance
         asFeet   = asMeters /~ foot :: Exp Double
     show (CPU.run (unit asFeet)) `shouldSatisfy` isInfixOf "6.5616"
-
-
-newtype Dangerous = Dangerous Int
-type instance MachineType Dangerous = MachineType Distance
-type instance Units Dangerous = Units Distance
--- Broken and unsafe instance of Newtype
-instance Newtype Dangerous Distance where
-  pack = unsafeCoerce
-  unpack = unsafeCoerce
-
-
-deriveQE [t|Dangerous -> Distance|]
-
-
